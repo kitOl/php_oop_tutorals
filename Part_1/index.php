ca2afe35edbd542b0bc5a7c67d68d4ab3b720c5e
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-01 20:01:23 +0300

    part 2 getters and setters and construct

--- Part_1/index.php
+++ Part_1/index.php
@@ -1,49 +1,52 @@
 <?php
 
-  class User {
+class User
+{
 
-    public $username = 'ivan';
-    public $email = 'ivan@ya.ru';
+  public $username = 'ivan';
+  public $email = 'ivan@ya.ru';
 
-    public function addFriend() {
-      return "$this->email added a new friend";
-    }
-
-
+  public function addFriend()
+  {
+    return "$this->email added a new friend";
   }
+}
 
-  $userOne = new User();
-  $userTwo = new User();
+$userOne = new User();
+$userTwo = new User();
 
-  echo "the class the object \$userOne is " . get_class($userOne) . "\n";
-  
-  echo "userOne $userOne->username\n";
-  echo $userOne->email . "\n";
-  echo $userOne->addFriend() . "\n";
-  
-  $userTwo->username = 'Oleg';
-  $userTwo->email = 'oleg@ya.ru';
-  echo "userTwo $userTwo->username \n";
-  echo $userTwo->email . "\n";
-  echo $userTwo->addFriend() . "\n";
-    
+echo "the class the object \$userOne is " . get_class($userOne) . "\n";
 
-  echo "features the Class:\n";
-  print_r(get_class_vars('User')); // features the Class
-  echo "methods the Class:\n";
-  print_r(get_class_methods('User')); // methods the Class
+echo "userOne $userOne->username\n";
+echo $userOne->email . "\n";
+echo $userOne->addFriend() . "\n";
+
+$userTwo->username = 'Oleg';
+$userTwo->email = 'oleg@ya.ru';
+echo "userTwo $userTwo->username \n";
+echo $userTwo->email . "\n";
+echo $userTwo->addFriend() . "\n";
+
+
+echo "features the Class:\n";
+print_r(get_class_vars('User')); // features the Class
+echo "methods the Class:\n";
+print_r(get_class_methods('User')); // methods the Class
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP OOP Tutoral</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP Tutoral</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
--- Part_2/index.php
+++ Part_2/index.php
@@ -1,49 +1,72 @@
 <?php
 
-  class User {
+class User
+{
 
-    public $username = 'ivan';
-    public $email = 'ivan@ya.ru';
+  public $username;
+  private $email;
 
-    public function addFriend() {
-      return "$this->email added a new friend";
-    }
-
-
+  public function __construct($username, $email)
+  {
+    $this->username = $username;
+    $this->email = $email;
   }
 
-  $userOne = new User();
-  $userTwo = new User();
+  public function addFriend()
+  {
+    return "$this->email added a new friend";
+  }
 
-  echo "the class the object \$userOne is " . get_class($userOne) . "\n";
-  
-  echo "userOne $userOne->username\n";
-  echo $userOne->email . "\n";
-  echo $userOne->addFriend() . "\n";
-  
-  $userTwo->username = 'Oleg';
-  $userTwo->email = 'oleg@ya.ru';
-  echo "userTwo $userTwo->username \n";
-  echo $userTwo->email . "\n";
-  echo $userTwo->addFriend() . "\n";
-    
+  public function getEmail()
+  {
+    return $this->email;
+  }
 
-  echo "features the Class:\n";
-  print_r(get_class_vars('User')); // features the Class
-  echo "methods the Class:\n";
-  print_r(get_class_methods('User')); // methods the Class
+  public function setEmail($email)
+  {
+    if (strpos($email, '@') > -1) {
+      $this->email = $email;
+    }
+  }
+}
+
+$userOne = new User('mario', 'mario@ya.ru');
+$userTwo = new User('dan', 'dan@ya.ru');
+
+echo "the class the object \$userOne is " . get_class($userOne) . "\n";
+
+echo "userOne $userOne->username\n";
+echo $userOne->getEmail() . "\n";
+echo $userOne->addFriend() . "\n";
+
+echo "userTwo $userTwo->username \n";
+echo $userTwo->getEmail() . "\n";
+echo $userTwo->addFriend() . "\n";
+
+
+echo "features the Class:\n";
+print_r(get_class_vars('User')); // features the Class
+echo "methods the Class:\n";
+print_r(get_class_methods('User')); // methods the Class
+
+$userOne->setEmail('nikita@mail.ru');
+echo "\n";
+echo "userOne email now is " . $userOne->getEmail(), "\n";
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP OOP Tutoral</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP Tutoral</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
