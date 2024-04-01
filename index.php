268039911e50bde4cec0294101a2a4a5483fee5a
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-01 09:04:44 +0300

    add starter code

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+  class User {
+
+    // properties methods
+
+  }
+
+  $userOne = new User();
+  $userTwo = new User();
+
+  echo "the class the object \$userOne is " . get_class($userOne) . "\n";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP Tutoral</title>
+</head>
+<body>
+    
+</body>
+</html>
